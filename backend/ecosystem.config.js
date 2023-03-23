185279fb4aefc4bfaa3d95e050ed7dbebdadc648
Sofia Klimova <klimovasonya@gmail.com>
2023-03-23 09:40:55 +0300

    1

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config({ path: '.env.deploy' });
 
 const {
-  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_FOLDER, DEPLOY_REF = 'origin/master',
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
 } = process.env;
 
 module.exports = {
@@ -17,8 +17,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: 'https://github.com/klmvsn/web-plus-pm2-deploy.git',
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
-      'post-deploy': `cd ~${DEPLOY_FOLDER}/source/backend && npm i && npm run build && pm2 restart ecosystem.config.js`,
+      'pre-deploy-local': `scp ./.env.deploy* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': `cd backend && npm i && npm run build && pm2 restart ecosystem.config.js --env production`,
     },
   },
 };
