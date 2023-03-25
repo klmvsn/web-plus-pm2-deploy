da086412d2ebe735597db5779b6f00f6389095a0
Sofia Klimova <klimovasonya@gmail.com>
2023-03-25 13:36:43 +0300

    fix cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -14,7 +14,14 @@ const app = express();
 mongoose.connect(DB_ADDRESS);
 
 // Только для локальных тестов. Не используйте это в продакшене
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://mesto-klimova.students.nomoredomains.work',
+    'http://mesto-klimova.students.nomoredomains.work',
+  ],
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
